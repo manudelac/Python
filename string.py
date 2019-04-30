789e340b11a68c331e54b09c36a4572cd367c662
manudelac <43617340+manudelac@users.noreply.github.com>
2019-04-30 22:37:24 +0200

    Add files via upload

---
+++ string.py
@@ -0,0 +1,18 @@
+#STRINGS
+nombre="Manuel"
+
+if (nombre=="Manuel"):
+    print ("Me llamo %s desde que nací."%nombre)
+else:
+    print ("No me llamo %s."%nombre)
+#Posición de una letra o del comienzo de un conjunto   
+print(nombre.index("el"))
+#número de caracteres de la string
+print(len(nombre))
+#numero de veces que aparece un caracter. Diferencia entre mayus y no
+print("Ana".count("a"))
+#imprime solo un trozo
+nombre2="Anal"
+print(nombre2[0:3])
+casa="casa "
+print(casa*4)
